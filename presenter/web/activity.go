d75d5cd7c56d4607ecbecfe84cb0d0850441817d
JeongWooYeong <wjd030811@gmail.com>
2021-03-04 11:14:35 +0900

    [FIX] apply calendar logic

--- presenter/web/activity.go
+++ presenter/web/activity.go
@@ -1,10 +1,9 @@
 package web
 
 import (
-	"excel-import/presenter/parser"
+	"excel-import/presenter/parser/excel"
+	"excel-import/presenter/web/exception"
 	"excel-import/usecase"
-	"excel-import/usecase/exception"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
@@ -12,17 +11,12 @@ import (
 )
 
 func setActivities(context *gin.Context) {
-	fmt.Println()
 	formData, e := context.FormFile("file")
 	if e != nil || filepath.Ext(strings.ToLower(formData.Filename)) != ".xlsx" {
-		exception.DefaultBadRequest(context)
-		return
+		exception.DefaultBadRequest(context) ; return
 	}
 
-	file, _ := formData.Open()
-	defer file.Close()
-
-	usecase.SetActivities(parser.Activity(file, formData.Size))
+	usecase.SetActivities(excel.ParseActivities(formData))
 
 	context.Status(http.StatusCreated)
 }

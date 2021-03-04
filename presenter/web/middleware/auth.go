d5da3c22c2a2a3903a2a948a66356e1aa7c20e0a
JeongWooYeong <wjd030811@gmail.com>
2021-03-04 23:51:38 +0900

    [ADD] authorization logic

---
+++ presenter/web/middleware/auth.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"os"
+	"pluto/presenter/web/exception"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		_, err := jwtAuth(context)
+
+		if err != nil {
+			switch err.Error() {
+			case exception.InvalidAuthorizationException:
+				exception.InvalidAuthorization(context)
+			}
+			return
+		}
+
+		context.Next()
+	}
+}
+
+func jwtAuth(context *gin.Context) (jwt.MapClaims, error) {
+	authorization := context.Request.Header.Get("Authorization")
+	if authorization[:7] != "Bearer " { return nil, fmt.Errorf(exception.InvalidAuthorizationException) }
+
+	token, err := jwt.Parse(authorization[7:], func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil { return nil, fmt.Errorf(exception.InvalidAuthorizationException) }
+
+	return token.Claims.(jwt.MapClaims), nil
+}

30b5ea9b687549dc9c17cec3a2b9244bc35736ae
JeongWooYeong <wjd030811@gmail.com>
2021-03-04 17:08:43 +0900

    [ADD] log export file

--- presenter/web/main.go
+++ presenter/web/main.go
@@ -2,10 +2,15 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
+	"os"
 	"pluto/presenter/web/middleware"
 )
 
 func Main() {
+	middleware.Logger.Out, _ = os.OpenFile("./resources/log/pluto.log",
+										   os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+										   0666)
+
 	router := gin.New()
 
 	router.Use(middleware.Log())

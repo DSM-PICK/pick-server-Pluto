ae922494981d79f90fd38a2f3a38455d86468aa4
JeongWooYeong <wjd030811@gmail.com>
2021-03-04 11:06:33 +0900

    [FIX] activity dto column name

--- usecase/dto/set_activity_request.go
+++ usecase/dto/set_activity_request.go
@@ -5,9 +5,9 @@ type SetActivityRequest struct {
 }
 
 type Activity struct {
-	Date string `json:"date"`
-	Schedule string `json:"schedule"`
+	Date                   string `json:"date"`
+	Weekday                string `json:"schedule"`
 	SecondFloorTeacherName string `json:"second_floor_teacher_name"`
-	ThirdFloorTeacherName string `json:"third_floor_teacher_name"`
+	ThirdFloorTeacherName  string `json:"third_floor_teacher_name"`
 	FourthFloorTeacherName string `json:"forth_floor_teacher_name"`
 }

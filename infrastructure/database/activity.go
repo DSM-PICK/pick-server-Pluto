c12a196e4350d9669e7fd45a5c918a6c6d503de8
JeongWooYeong <wjd030811@gmail.com>
2021-03-05 09:52:48 +0900

    [FIX] data to update

--- infrastructure/database/activity.go
+++ infrastructure/database/activity.go
@@ -1,10 +1,19 @@
 package database
 
-import "pluto/entity"
+import (
+	"gorm.io/gorm/clause"
+	"pluto/entity"
+)
 
 var ActivityRepository entity.ActivityRepository = Initialize()
 
 func (db *Database) CreateActivity(activity entity.Activity) entity.Activity {
-	db.connection.FirstOrCreate(&activity)
+	db.connection.Clauses(clause.OnConflict{
+		Columns: []clause.Column{{Name: "date"}},
+		DoUpdates: clause.AssignmentColumns([]string{"schedule",
+											 		 "second_floor_teacher_id",
+											 		 "third_floor_teacher_id",
+													 "forth_floor_teacher_id"}),
+	}).Create(&activity)
 	return activity
 }
--- infrastructure/database/student.go
+++ infrastructure/database/student.go
@@ -1,10 +1,20 @@
 package database
 
-import "pluto/entity"
+import (
+	"gorm.io/gorm/clause"
+	"pluto/entity"
+)
 
 var StudentRepository entity.StudentRepository = Initialize()
 
 func (db *Database) CreateStudent(student entity.Student) entity.Student {
-	db.connection.FirstOrCreate(&student)
+	db.connection.Clauses(clause.OnConflict{
+		Columns: []clause.Column{{Name: "num"}},
+		DoUpdates: clause.AssignmentColumns([]string{"name",
+													 "club_name",
+													 "class_name",
+													 "is_monday_self_study",
+													 "is_tuesday_self_study"}),
+	}).Create(&student)
 	return student
 }
